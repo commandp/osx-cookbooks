57f02f9d820a2ad713b016884981ee9fb55895c2
Sammy Lin <sammylintw@gmail.com>
2015-04-22 17:01:19 +0800

    mark autologin

--- cookbooks/commandp/recipes/default.rb
+++ cookbooks/commandp/recipes/default.rb
@@ -5,13 +5,13 @@
 
 include_recipe "mac_os_x"
 
-## 自動登入 commandp
-mac_os_x_userdefaults "自動登入 #{node['user']}" do
-  domain "/Library/Preferences/com.apple.loginwindow"
-  key "autoLoginUser"
-  value "#{node['user']}"
-  sudo true
-end
+## 自動登入 commandp (Bug)
+# mac_os_x_userdefaults "自動登入 #{node['user']}" do
+#   domain "/Library/Preferences/com.apple.loginwindow"
+#   key "autoLoginUser"
+#   value "#{node['user']}"
+#   sudo true
+# end
 
 mac_os_x_userdefaults "系統偏好設定 / 安全性與隱私, 取消喚醒輸入密碼" do
   domain "com.apple.screensaver"
